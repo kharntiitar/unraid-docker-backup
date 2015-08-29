a70104f6d000d67e161872c9f7add93b9c8cac6e
kharntiitar <kharntiitar@gmail.com>
2015-08-29 17:44:53 +0800

    Cleaned up code. Created a directory to find the configs (oops). Fixed usage of subprocess when working with docker commands. Renamed 'constants' to 'variables'. Fixed now variable. All working well at present.

--- udb/main.py
+++ udb/main.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python
 
-
 # Imports
 import os, re, subprocess, datetime
 
 ##### EDIT ME CONFIG ######
 backuproot = "/mnt/user/Backups/"
-
+configroot = "/mnt/cache/config/"
 # Constants
-now = datetime.datetime.now
-date = str(now.year)+str(now.month)+str(now.day)+"-"+str(now.hour)+str(now.minute)+"-"
+now = datetime.datetime.now()
+date = str(now.year)+str(now.month)+str(now.day)+"-"+str(now.hour)+str(now.minute)
 backupdir = backuproot + date
 docker_images =  re.findall("\/(\S+)", os.popen("docker inspect --format='{{.Name}}' $(sudo docker ps -q --no-trunc)").read())
 
-
-
 # Make the backup directory
-subprocess.call(["mkdir -p", backupdir])
-subprocess.call(["mkdir -p", backupdir + "Docker Config " + date])
+subprocess.call(["mkdir", "-p",  backupdir])
 
 # Stop and docker images and tar the config files, then start the image again.
 for image in docker_images:
-    tarball = date + image + ".tar.gz"
-    subprocess.call(["docker stop", image])
-    subprocess.call(["tar -pczf", backupdir + "/" + tarball, image])
-    subprocess.call(["docker start", image])
+    tarball = date + "." + image + ".tar.gz"
+    subprocess.call(["docker", "stop", image])
+    subprocess.call(["tar", "-pczf", backupdir + "/" + tarball, configroot + image.lower()])
+    subprocess.call(["docker", "start", image])
 
 
 
